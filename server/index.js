70f4145f9b95437160a3905f1646072b2f42ca1d
Mehmet Efe Çağlar <efecagglar@gmail.com>
2024-01-15 23:14:28 +0200

    server/index.js update

--- server/index.js
+++ server/index.js
@@ -16,12 +16,13 @@ app.use(cors({
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// })
 
 
 app.use('/posts', postRoutes); // For http://local.host:4444/posts
