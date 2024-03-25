6c404810d2319d0acd4f2552f006b477f3e26e74
Mehmet Efe Çağlar <efecagglar@gmail.com>
2024-03-25 17:58:21 +0200

    .env

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -9,7 +9,7 @@ const Posts = ({ setCurrentId }) => {
     const { posts, isLoading } = useSelector((state) => state.posts);
     const classes = useStyles();
 
-    if (!posts.length && !isLoading) return 'No posts found.'
+    if (!posts || (posts && !posts.length && !isLoading)) return 'No posts found.'
 
     return (
         isLoading ?     
